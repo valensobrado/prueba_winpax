d77d8f99af8b6b05812f629309aa44f72dc33272
Valentin Sobrado Carnevali <sobradovalentin@gmail.com>
2022-06-07 13:07:27 -0300

    Correcciones realizadas

--- backend.php
+++ backend.php
@@ -6,24 +6,39 @@ if(isset($_POST['btn'])){
             $name = trim($_POST['name']);
             $surname = trim($_POST['surname']);
             $email = $_POST['email'];
+            $valid_email = filter_var($email, FILTER_VALIDATE_EMAIL);
             $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
-            $db_winpx = mysqli_connect("localhost", "root", "", "prueba_winpax");
+            $db_winpax = mysqli_connect("localhost", "root", "", "prueba_winpax");
+            $busqueda_email = "SELECT * FROM users WHERE email = '$email'";
+            $email = mysqli_real_escape_string($db_winpax, $email);
+            $respuesta_email = mysqli_query($db_winpax, $busqueda_email);
+            $filas = mysqli_num_rows($respuesta_email);
+
+            if(($valid_email == true ) && (!$filas)){
+                 echo "Email válido!";   
+            }else{
+                ?>
+                    <h3>El email ingrsado no corresponde con un formato válido o ya está registrado en el sistema</h3>
+                    <h3>Intenta con uno otro email!</h3>
+                    <button><a href="index.php">Intentar con otro!</a></button> 
+                <?php
+                die();
+            }
+
+            
             $consulta = "INSERT INTO users(name, surname, email, password) VALUES ('$name', '$surname', '$email','$password')";
-            $resultado = mysqli_query($db_winpx, $consulta);
+            $name = mysqli_real_escape_string($db_winpax, $name);
+            $surname = mysqli_real_escape_string($db_winpax, $surname);
+            $email = mysqli_real_escape_string($db_winpax, $email);
+            $password = mysqli_real_escape_string($db_winpax, $password);
+            $resultado = mysqli_query($db_winpax, $consulta);
 
             if($resultado){
                 ?>
                 <h3>¡Te has inscripto correctamente!</h3>
                 <button><a href="index.php">Cargar otro usuario</a></button>
                 <?php
-            }else{
-                ?>
-            <h3>El email ingresado ya está registrado en nuestro sistema
-                <button><a href="index.php">Intentar con otro!</a></button>
-                
-            </h3>
-            <?php
             }
         }else{
             ?>
--- index.php
+++ index.php
@@ -13,10 +13,13 @@
         <h2>Registrar usuario nuevo:</h2>
         <input name="name" type="text" placeholder="Nombre" required>
         <input name="surname" type="text" placeholder="Apellido" required>
-        <input name="email" type="email" placeholder="Correo" required>
+        <input name="email" type="text" placeholder="Correo" required>
         <input name="password" type="password" placeholder="Crear contraseña" required>
         <input name="second-password" type="password" placeholder="Repetir contraseña"required>
         <input name="btn" type="submit" value="Registrar">
     </form>
+    <?php
+        include('backend.php');
+    ?>
 </body>
 </html>
